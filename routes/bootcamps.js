78080438ddf3ef34ef6f1ac6af1a17dbbbd61f96
Iamkosgei <kosgeik2930@gmail.com>
2020-03-12 13:39:50 +0300

    connect to mongodb and bootcamps crud

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const { getBootcamps } = require('../controllers/bootcamps');
+const {
+  getBootcamps,
+  CreateBootcamps,
+  getBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require('../controllers/bootcamps');
 
 const router = express.Router();
 
 router.route('/').get(getBootcamps);
+router.route('/').post(CreateBootcamps);
+router.route('/:id').get(getBootcamp);
+router.route('/:id').put(updateBootcamp);
+router.route('/:id').delete(deleteBootcamp);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-//route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
+//connect to database
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
 // app.use(logger);
+
+//Body parser
+app.use(express.json());
 
 //Dev logging middleware
 if (process.env.NODE_ENV == 'development') {
@@ -20,9 +27,15 @@ if (process.env.NODE_ENV == 'development') {
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandles promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
