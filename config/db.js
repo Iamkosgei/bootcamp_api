23cd22cb35e18594414afa6b5e736ab235a62b34
Iamkosgei <kosgeik2930@gmail.com>
2020-03-13 11:23:44 +0300

    mongodb error handling

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true
-  });
-
-  console.log(`MongoDB Connected ${conn.connection.host}`);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Connected ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDB Error ${error}`);
+  }
 };
 module.exports = connectDB;
